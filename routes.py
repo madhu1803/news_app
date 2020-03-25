59ca51c11817360293f96421bc42538ee333a20a
madhumithaa <madhumithaa1411@gmail.com>
2020-03-25 18:54:04 +0530

    routes done

--- routes.py
+++ routes.py
@@ -1,7 +1,26 @@
-"""Define the routes here"""
+"""Define the Views here"""
 
 from app import app
 from views import *
 
-# mapping for routes and views
-app.add_url_rule("/", view_func=HomeRoute.as_view("home"))
+# mapping for Views and views
+app.add_url_rule("/", view_func=HomeView.as_view("home"))
+app.add_url_rule("/admin/login", view_func=LoginView.as_view("admin_login"))
+app.add_url_rule("/admin/signup", view_func=SignupView.as_view("admin_signup"))
+app.add_url_rule(
+    "/admin/post/new", view_func=PostCreateView.as_view("post_create_view")
+)
+app.add_url_rule("/admin/post/all", view_func=PostListView.as_view("post_list_view"))
+app.add_url_rule(
+    "/admin/post/id/view", view_func=PostDetailView.as_view("post_detail_view")
+)
+app.add_url_rule(
+    "/admin/post/id/edit", view_func=PostEditView.as_view("post_ddit_view")
+)
+app.add_url_rule(
+    "/admin/post/id/delete", view_func=PostDeleteView.as_view("post_delete_view")
+)
+app.add_url_rule("/post/all", view_func=UserPostListView.as_view("user_post_List_view"))
+app.add_url_rule(
+    "/post/id/view", view_func=UserPostDetailView.as_view("user_post_detail_view")
+)
--- views.py
+++ views.py
@@ -3,8 +3,71 @@
 from flask.views import MethodView
 
 
-class HomeRoute(MethodView):
+class HomeView(MethodView):
     """Landing page for user"""
 
     def get(self):
         return "hello world"
+
+
+class LoginView(MethodView):
+    """Login page for admin"""
+
+    def get(self):
+        return "admin login"
+
+
+class SignupView(MethodView):
+    """signup page for admin"""
+
+    def get(self):
+        return "admin signup"
+
+
+class PostCreateView(MethodView):
+    """create post for admin"""
+
+    def get(self):
+        return "admin post create"
+
+
+class PostListView(MethodView):
+    """view post for admin"""
+
+    def get(self):
+        return "admin post view"
+
+
+class PostEditView(MethodView):
+    """edit post for admin"""
+
+    def get(self):
+        return "admin post edit"
+
+
+class PostDeleteView(MethodView):
+    """delete post for admin"""
+
+    def get(self):
+        return "admin post delete"
+
+
+class PostDetailView(MethodView):
+    """delete post for admin"""
+
+    def get(self):
+        return "admin post view details"
+
+
+class UserPostListView(MethodView):
+    """User Post List View"""
+
+    def get(self):
+        return "User Post List View"
+
+
+class UserPostDetailView(MethodView):
+    """User Post Detail View"""
+
+    def get(self):
+        return "User Post Detail View"
