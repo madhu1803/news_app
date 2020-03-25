228817bacd4df3901ab7e9f70fe403b7b6ee7a1b
madhumithaa <madhumithaa1411@gmail.com>
2020-03-25 19:31:44 +0530

    templates init

--- views.py
+++ views.py
@@ -1,73 +1,74 @@
 """Define the views and view based operations here"""
 
 from flask.views import MethodView
+from flask import render_template
 
 
 class HomeView(MethodView):
     """Landing page for user"""
 
     def get(self):
-        return "hello world"
+        return render_template("home.html")
 
 
 class LoginView(MethodView):
     """Login page for admin"""
 
     def get(self):
-        return "admin login"
+        return render_template("login_view.html")
 
 
 class SignupView(MethodView):
     """signup page for admin"""
 
     def get(self):
-        return "admin signup"
+        return render_template("signup_view.html")
 
 
 class PostCreateView(MethodView):
     """create post for admin"""
 
     def get(self):
-        return "admin post create"
+        return render_template("post_create_view.html")
 
 
 class PostListView(MethodView):
     """view post for admin"""
 
     def get(self):
-        return "admin post view"
+        return render_template("post_list_view.html")
 
 
 class PostEditView(MethodView):
     """edit post for admin"""
 
     def get(self):
-        return "admin post edit"
+        return render_template("post_edit_view.html")
 
 
 class PostDeleteView(MethodView):
     """delete post for admin"""
 
     def get(self):
-        return "admin post delete"
+        return render_template("post_delete_view.html")
 
 
 class PostDetailView(MethodView):
     """delete post for admin"""
 
     def get(self):
-        return "admin post view details"
+        return render_template("post_detail_view.html")
 
 
 class UserPostListView(MethodView):
     """User Post List View"""
 
     def get(self):
-        return "User Post List View"
+        return render_template("user_post_view.html")
 
 
 class UserPostDetailView(MethodView):
     """User Post Detail View"""
 
     def get(self):
-        return "User Post Detail View"
+        return render_template("user_post_detail_view.html")
